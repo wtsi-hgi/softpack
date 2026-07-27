17e1783ceef268886d43ffba95d02a91527ccd1b
Michael Woolnough <mw31@sanger.ac.uk>
2026-07-27 11:17:14 +0100

    New build code

---
+++ build/server.go
@@ -0,0 +1,90 @@
+package build
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"path"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func startServer(aptSrc string) (net.Listener, error) {
+	u, err := url.Parse(aptSrc)
+	if err != nil {
+		return nil, err
+	}
+
+	var h http.Handler
+
+	switch u.Scheme {
+	case "s3":
+		if h, err = newS3Proxy(u); err != nil {
+			return nil, err
+		}
+	case "http", "https":
+		h = httputil.NewSingleHostReverseProxy(u)
+	default:
+		h = http.FileServer(http.Dir(aptSrc))
+	}
+
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return nil, err
+	}
+
+	go http.Serve(l, h)
+
+	return l, nil
+}
+
+type s3Proxy struct {
+	host   string
+	path   string
+	client *s3.Client
+}
+
+func newS3Proxy(u *url.URL) (http.Handler, error) {
+	u.Path = strings.TrimPrefix(u.Path, "/")
+
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("failed to load S3 configuration, %w", err)
+	}
+
+	return &s3Proxy{
+		host:   u.Host,
+		path:   u.Path,
+		client: s3.NewFromConfig(cfg),
+	}, nil
+}
+
+func (s *s3Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p := strings.TrimPrefix(path.Join(s.path, r.URL.Path), "/")
+
+	obj, err := s.client.GetObject(context.Background(), &s3.GetObjectInput{
+		Bucket: &s.host,
+		Key:    &p,
+	})
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			http.NotFound(w, r)
+
+			return
+		}
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	defer obj.Body.Close()
+
+	io.Copy(w, obj.Body)
+}
