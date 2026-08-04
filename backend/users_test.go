6ceac492f5f348d4c8e46316fcc2b0b4ba72f3f2
Sky Haines-Bass <sh55@sanger.ac.uk>
2026-08-04 08:23:57 +0000

    fix tests

--- backend/users_test.go
+++ backend/users_test.go
@@ -2,6 +2,7 @@ package backend
 
 import (
 	"net/http"
+	"os/user"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,8 +10,13 @@ import (
 
 func TestGetGroups(t *testing.T) {
 	s := newTestServer(t)
+	u, err := user.Current()
+	assert.NoError(t, err)
 
-	code, resp := getResponse(t, s, "/groups", "mercury")
+	code, resp := getResponse(t, s, "/groups", u.Username)
+	grp, err := user.LookupGroupId(u.Gid)
+	assert.NoError(t, err)
+
 	assert.Equal(t, http.StatusOK, code)
-	assert.Contains(t, resp, "hgi")
+	assert.Contains(t, resp, grp.Name)
 }
