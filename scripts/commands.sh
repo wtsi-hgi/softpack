0c558fcca950dce0aaa5b0fd30a6af85baf2a629
Michael Woolnough <mw31@sanger.ac.uk>
2026-09-07 11:15:45 +0000

    Updated

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -5,12 +5,17 @@ __args=( "$@" );
 commands() {
 	case "$#" in
 	0)
-		declare solo=1;
+		declare solo="";
 		declare part="";
 
-		__args=( "" "${__args[@]}" );
+		case "${__args[0]:-}" in
+		"--completions"|"--generate-roff"|"--man-page")
+			;;
+		*)
+			__args=( "$part" "${__args[@]}" );;
+		esac;
 
-		eval "__parts() { [ -n \"\$part\" ] && echo ${part@Q}; }; __sections() { [ -n \"\$part\" ] && cat ${0@Q}; }";;
+		eval "__parts() { [ -n \"\$part\" ] && echo -e ${part@Q}; }; __sections() { [ -n \"\$part\" ] && cat ${0@Q}; }";;
 	1)
 		declare start="${1:?Start string required}";
 
@@ -18,7 +23,6 @@ commands() {
 	*)
 		declare general="${1:-}";
 		shift;
-		declare base="$(dirname "$0")/";
 		declare -A sections=();
 
 		for section; do
@@ -27,7 +31,7 @@ commands() {
 
 		printf -v list "%s\n" "${!sections[@]}";
 
-		eval "__parts() { echo -en ${list@Q}; }; __sections() { (cd ${base@Q};case \${part:-} in \"\")$(
+		eval "__parts() { echo -en ${list@Q}; }; __sections() { case \${part:-} in \"\")$(
 			if [ -n "$general" ]; then
 				echo -n "cat ${general@Q}";
 			fi;
@@ -35,7 +39,7 @@ commands() {
 			for part in "${!sections[@]}"; do
 				echo -n "${part@Q})cat ${sections[$part]@Q};;";
 			done;
-		)esac) }";;
+		)esac; }";;
 	esac;
 
 	__handle_parts "${__args[@]}";
@@ -49,6 +53,264 @@ __flags() {
 	while read line; do
 		printf "%s\0%s\0%s\0" "$(sed -e 's/  \+/ /g; s/^\([^ ]*\).*/\1/' <<< "$line")" "$(sed -e 's/ \+#.*//; s/  +/ /g' <<< "$line" | cut -s -d' ' -f2)" "$(sed -n 's/.* # *//p' <<< "$line")";
 	done < <(__sections | sed -n '/^#/d; /^: /!q; s/^:  *//p');
+}
+
+__flag_type() {
+	declare type="$(sed -e 's/\[\]$//; s/^\[\(.*\)\]$/\1/; s/#*$//' <<< "$1")";
+
+	if [ "$type" != "" ]; then
+		printf " %s" "$type";
+	fi;
+}
+
+__print_flags_usage() {
+	while read -r -d '' flag && read -r -d '' type && read -r -d '' desc; do
+		flag="${flag%,*}";
+
+		if [ "$flag" = "..." -o "$flag" = "…" ]; then
+			additional="1";
+			additionalDesc="$desc";
+		elif [ "${type: -1}" = "]" -o "$type" = "" ]; then
+			echo -n " [${1+\\fI}$flag${1+\\fR}$(__flag_type "${type:-}")]";
+
+			if [ "${type: -2}" = "[]" ]; then
+				echo -n "...";
+			fi;
+		else
+			printf " %s%s" "${1+\\fI}$flag${1+\\fR}" "$(__flag_type "$type")";
+		fi;
+	done < <(__flags);
+}
+
+__generate_roff() {
+	declare cmd="$(basename "$0")";
+	declare desc="$(__description)";
+	declare additional="";
+	declare additionalDesc="";
+
+	echo ".TH ${cmd^^} 1";
+	echo ".SH NAME";
+	echo "$cmd";
+	echo ".SH SYNOPSIS";
+	echo ".B $cmd";
+
+	if [ -v solo ]; then
+		echo -n "\f";
+		__print_flags_usage "";
+	else
+		echo -n "\fIsubcommand\fR";
+		__print_flags_usage "";
+		echo -n " [\fIsubcommand_flags\fR]";
+	fi;
+
+	echo " ${additional:+[\fIARGS\fR]...}";
+	echo -e "${desc:+.SH DESCRIPTION\n$desc}";
+
+	if [ ! -v solo ]; then
+		echo ".SH SUBCOMMANDS";
+
+		while read part; do
+			declare desc="$(__description)";
+
+			echo -e ".TP\n.B $part${desc+\n$desc}";
+		done < <(__parts);
+	fi;
+
+	if __flags | grep -q .; then
+		echo ".SH ${solo-GLOBAL }FLAGS";
+
+		while read -r -d '' flag && read -r -d '' type && read -r -d '' desc; do
+			if [ "$flag" = "..." -o "$flag" = "…" ]; then
+				continue;
+			fi;
+
+			echo -en ".TP\n.B ";
+			sed -e 's/,/, /g; s/#//g' <<< "$flag$(__flag_type "$type")";
+			echo "$desc";
+		done < <(__flags);
+	fi;
+
+	if [ -n "$additionalDesc" ]; then
+		echo ".SH ADDITIONAL ARGUMENTS";
+		echo "$additionalDesc";
+	fi;
+
+	if [ ! -v solo ]; then
+		declare additional="";
+		declare additionalDesc="";
+
+		while read part; do
+			declare desc="$(__description)";
+
+			echo -e ".ce 1\n.SH SUBCOMMAND: $part\n.SH SYNOPSIS";
+			echo -n ".B $cmd \fI$part\fR [\fIglobal_flags\fR]";
+			__print_flags_usage "";
+			echo " ${additional:+[\fIARGS\fR]...}";
+
+			if [ -n "$desc" ]; then
+				echo -e ".SH DESCRIPTION\n$desc";
+			fi;
+
+			if __flags | grep -q .; then
+				echo ".SH FLAGS";
+
+				while read -r -d '' flag && read -r -d '' type && read -r -d '' desc; do
+					if [ "$flag" = "..." -o "$flag" = "…" ]; then
+						continue;
+					fi;
+
+					echo -e ".TP\n.B ";
+					sed -e 's/,/, /g; s/#//g' <<< "$flag$(__flag_type "$type")";
+					echo "$desc";
+				done < <(__flags);
+			fi;
+		done < <(__parts);
+
+		if [ -n "$additionalDesc" ]; then
+			echo ".SH ADDITIONAL ARGUMENTS";
+			echo "$additionalDesc";
+		fi;
+	fi;
+}
+
+__flag_completion() {
+	while read -r -d '' flag && read -r -d '' type && read -r -d ''; do
+		if [ "$flag" = "..." -o "$flag" = "…" ]; then
+			hasExtra=true;
+		else
+			declare t=" ";
+
+			if [ "${type: -2}" = "[]" ]; then
+				type="${type:0:-2}";
+				t="a";
+			elif [ "${type:0:1}" = "[" -a "${type: -1}" = "]" ]; then
+				type="${type:1:-1}";
+			fi;
+
+			if [ -z "$type" ]; then
+				t+="!";
+			elif [ "${type: -1}" != "#" ]; then
+				t+="-f";
+			fi;
+
+			echo -n "[${flag@Q}]=${t@Q} ";
+		fi;
+	done < <(__flags);
+}
+
+__restrict_completion() {
+	cat <<HEREDOC
+$1	declare onValue="$([ -v solo ] && echo "false" || echo "true")";
+$1	declare isAny="false";
+
+$1	for arg in "\${COMP_WORDS[@]:1:\$(( \$COMP_CWORD - 1 ))}"; do
+$1		declare flag="\${opts[\$arg]}";
+
+$1		if \$onValue || [ -z "\${flag:-}" ]; then
+$1			onValue=false;
+
+$1			continue;
+$1		fi;
+
+$1		if [ "\${flag:0:1}" = ' ' ]; then
+$1			unset opts[\$arg];
+$1		fi;
+
+$1		if [ "\${flag:1}" = '!' ]; then
+$1			continue;
+$1		fi;
+
+$1		onValue=true;
+$1		isAny="\${flag:1}";
+$1	done;
+
+$1	if \$onValue; then
+$1		opts=();
+$1		hasExtra="\$isAny";
+$1	fi;
+HEREDOC
+}
+
+__completions() {
+	declare hasV="$(compgen -V var -W "a" "" &> /dev/null && echo true || echo false)";
+	declare fn="__completions";
+	declare hasExtra=false;
+
+	while declare -F "$fn" > /dev/null; do
+		fn="__do_completion_$RANDOM";
+	done;
+
+	echo -en "$fn() {\n\tdeclare -A opts=( ";
+
+	if [ -v solo ]; then
+		__flag_completion;
+	fi;
+
+	echo -en ");\n\tdeclare hasExtra=\"";
+
+	if $hasExtra; then
+		echo -n "-f";
+	fi;
+
+	echo -e "\";";
+	if [ -v solo ]; then
+		__restrict_completion "";
+		echo;
+	else
+		echo -en "\n\tif [ \$COMP_CWORD -eq 1 ]; then\n\t\topts=( ";
+
+		while read part; do
+			echo -n "[${part@Q}]='' ";
+		done < <(__parts);
+
+		echo -e ");\n\telse";
+
+		if [ "$(__flags | wc -c)" -gt 0 ]; then
+			echo -en "\t\topts=( ";
+
+			__flag_completion;
+
+			echo -e ");";
+		fi;
+
+		declare primaryHasExtra="$hasExtra";
+
+		if $hasExtra; then
+			echo -e "\t\thasExtra=\"-f\";";
+		fi;
+
+		echo -e "\t\tcase \"\${COMP_WORDS[1]}\" in";
+
+		while read part; do
+			echo -en "\t\t${part@Q})\n\t\t\topts+=( ";
+
+			__flag_completion;
+
+			echo -n ");";
+
+			if $hasExtra && ! $primaryHasExtra; then
+				echo -en "\n\t\t\thasExtra=\"-f\";";
+			fi;
+
+			echo ";";
+		done < <(__parts);
+
+		echo -e "\t\tesac;\n";
+
+		__restrict_completion "	";
+
+		echo -e "\tfi;\n";
+	fi;
+
+	cat <<HEREDOC
+	if [ -n "\$hasExtra" -o \${#opts[@]} -gt 0 ]; then
+		$($hasV || echo -n "read -d '\\n' -a COMPREPLY < <(")compgen$($hasV && echo -n " -V COMPREPLY" || true) \${opts[@]+ -W "\${!opts[*]}"} \${hasExtra:---}\${hasExtra:+ --} "\${COMP_WORDS[\$COMP_CWORD]}"$($hasV || echo -n ")");
+		readarray -t COMPREPLY < <(printf '%s\\n' "\${COMPREPLY[@]}" | LC_ALL=C sort);
+	fi;
+}
+
+complete -F ${fn@Q} ${0@Q};
+HEREDOC
 }
 
 __print_flags() {
@@ -70,7 +332,7 @@ __print_flags() {
 
 		while read -r -d '' flag && read -r -d '' type && read -r -d '' desc; do
 			if [ -n "$desc" -a "$flag" != "..." -a "$flag" != "…" ]; then
-				printf "  %-${maxLength}s" "$flag"
+				printf "  %-${maxLength}s" "$flag";
 
 				if [ -n "$desc" ]; then
 					echo -n " ";
@@ -84,39 +346,18 @@ __print_flags() {
 
 __usage() {
 	declare part="${1:-}";
-	declare additional=false;
+	declare additional="";
 	declare additionalDesc="";
 
-	echo -n "Usage: $0 [--help] ${part:-SUBCOMMAND}";
+	echo -n "Usage: $0 [--help]${solo- ${part:-SUBCOMMAND}}";
 
-	while read -r -d '' flag && read -r -d '' type && read -r -d '' desc; do
-		flag="${flag%,*}";
-
-		if [ "$flag" = "..." -o "$flag" = "…" ]; then
-			additional=true;
-			additionalDesc="$desc";
-		elif [ "${type: -1}" = "]" -o "$type" = "" ]; then
-			echo -n " [$flag$(__flag_type "${type:-}")]";
-
-			if [ "${type: -2}" = "[]" ]; then
-				echo -n "...";
-			fi;
-		else
-			printf " %s%s" "$flag" "$(__flag_type "$type")";
-		fi;
-	done < <(
-		if [ -n "$part" ]; then
-			part="" __flags;
-		fi;
-
-		__flags;
-	);
-
-	if $additional; then
-		echo " ARGS";
-	else
-		echo;
+	if [ -n "$part" ]; then
+		part="" __print_flags_usage;
 	fi;
+
+	__print_flags_usage;
+
+	echo "${additional:+ ARGS}";
 
 	__description;
 
@@ -145,14 +386,6 @@ __help() {
 	__print_flags;
 }
 
-__flag_type() {
-	declare type="$(sed -e 's/\[\]$//; s/^\[\(.*\)\]$/\1/; s/#*$//' <<< "$1")";
-
-	if [ "$type" != "" ]; then
-		printf " %s" "$type";
-	fi;
-}
-
 __section_help() {
 	__usage "$part";
 	__print_flags "$part";
@@ -170,11 +403,24 @@ __bind_flags() {
 }
 
 __handle_parts() {
-	if [ "${1:-}" = "--help" ]; then
+	case "${1:-}" in
+	"--help")
 		__help;
 
-		exit 0;
-	fi;
+		exit 0;;
+	"--completions")
+		__completions;
+
+		exit 0;;
+	"--generate-roff")
+		__generate_roff;
+
+		exit 0;;
+	"--man-page")
+		man -l <(__generate_roff);
+
+		exit 0;;
+	esac;
 
 	if [ -z "${1:-}" ]; then
 		{
@@ -344,7 +590,7 @@ __handle_parts() {
 		fi;
 	done;
 
-	if [ -n "${solo:-}" ]; then
+	if [ -v solo ]; then
 		eval "$(__bind_flags)";
 
 		return 0;
