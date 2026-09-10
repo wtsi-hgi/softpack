2d4f6f0ee35cdcdb2b56aa7da33593571bbcf654
Michael Woolnough <mw31@sanger.ac.uk>
2026-09-10 12:55:56 +0000

    Set correct bash format for vim

--- scripts/global.sh
+++ scripts/global.sh
@@ -4,7 +4,7 @@
 set -euo pipefail;
 
 declare BASE=/repo/pool/main/binary-amd64/;
-declare maintainer="hgi";
+declare maintainer="hgi <hgi@sanger.ac.uk>";
 
 aptConf() {
 	cat <<HEREDOC
@@ -254,20 +254,6 @@ fixPythonVersioning() {
 		"XB-Softpack" "true";
 }
 
-addPythonAlias() {
-	declare file="$1";
-
-	if [ "$(dpkg-deb -f "$file" Maintainer)" != "$maintainer" ]; then
-		return 0;
-	fi;
-
-	declare name="$(dpkg-deb -f "$file" Package)";
-
-	setMetadata "$file" \
-		"XB-Alias" "py${name:6}" \
-		"XB-Softpack" "true";
-}
-
 installDebs() {
 	declare debDir="$1";
 
@@ -289,8 +275,6 @@ installDebs() {
 			patchSystemd "$file";
 		elif [ "${file:0:8}" = "python3." ]; then
 			fixPythonVersioning "$file";
-		elif [ "${file:0:8}" = "python3-" ]; then
-			addPythonAlias "$file";
 		elif [ "${file:0:12}" = "r-base-core_" ]; then
 			addRSymlinkIfOpt "$file";
 			fixR "$file" "r";
@@ -308,4 +292,4 @@ installDebs() {
 	shopt -u nullglob;
 }
 
-# vim: set filetype=sh :
+# vim: set filetype=bash :
